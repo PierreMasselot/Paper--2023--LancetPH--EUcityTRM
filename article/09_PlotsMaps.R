5224c05f4b7a5437897cf77e680325dad4f652a8
Pierre Masselot <pierre.masselot@lshtm.ac.uk>
2021-08-17 10:02:56 +0100

    Update 09_PlotsMaps.R

--- article/09_PlotsMaps.R
+++ article/09_PlotsMaps.R
@@ -16,7 +16,9 @@ basic_map <- ggplot(data = subset(cityres, agegroup == agelabs[3]),
   aes(x = lon, y = lat, size = pop)) + 
   theme_void() + 
   geom_sf(data = euromap, fill = grey(.95), inherit.aes = F, col = grey(.5)) + 
-  coord_sf(xlim = urauext[c(1,3)], ylim = urauext[c(2,4)]) +
+  coord_sf(xlim = urauext[c(1,3)], ylim = urauext[c(2,4)],
+    crs = sf::st_crs(3035), default_crs = sf::st_crs(4326),
+    lims_method = "box") +
   scale_size(name = "Population", range = c(.5, 5),
     guide = "none") + 
   theme(legend.position = "bottom", legend.box = "vertical") + 
